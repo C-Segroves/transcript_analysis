2f59b43975f5ea7ea46995d0fa0503e448c243cc
C-Segroves <christopher.n.segroves@gmail.com>
2025-04-21 22:13:31 -0400

    fixing linux machine name references

--- build client image.sh
+++ build client image.sh
@@ -1,7 +1,10 @@
+# filepath: /c:/Users/Chris/Desktop/git/transcript analysis/transcript_analysis_server/build client image.sh
 #!/bin/bash
 echo "Building Client Image..."
 
-# Set the SERVER_HOST environment variable to the current hostname
+# Set the SERVER_HOST environment variable (passed as an argument or default to a known IP)
+SERVER_HOST=${1:-192.168.1.108}  # Replace with the actual server IP if known
+export SERVER_HOST
 export MACHINE_NAME=$(hostname)
 
 cd /path/to/transcript_analysis_server
@@ -10,7 +13,7 @@ docker build -t my-client-image -f client/Dockerfile .
 if [ $? -eq 0 ]; then
     echo "Client image built successfully!"
     echo "Running Client Image..."
-    docker run -it --name client-container -e SERVER_HOST=192.168.1.108  machine_name=$MACHINE_NAME my-client-image
+    docker run -it --name client-container -e SERVER_HOST=$SERVER_HOST -e MACHINE_NAME=$MACHINE_NAME my-client-image
 else
     echo "Failed to build client image. Check the output for errors."
     exit 1
