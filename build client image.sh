d0c24dae99054ac7b19cbfc0da2063b83ec220e2
C-Segroves <christopher.n.segroves@gmail.com>
2025-04-21 22:18:43 -0400

    adding a pause before the .sh file launches

--- build client image.sh
+++ build client image.sh
@@ -1,4 +1,3 @@
-# filepath: /c:/Users/Chris/Desktop/git/transcript analysis/transcript_analysis_server/build client image.sh
 #!/bin/bash
 echo "Building Client Image..."
 
@@ -13,8 +12,13 @@ docker build -t my-client-image -f client/Dockerfile .
 if [ $? -eq 0 ]; then
     echo "Client image built successfully!"
     echo "Running Client Image..."
-    docker run -it --name client-container -e SERVER_HOST=$SERVER_HOST -e machine_name=$MACHINE_NAME my-client-image
+    docker run -it --name client-container -e SERVER_HOST=$SERVER_HOST -e MACHINE_NAME=$MACHINE_NAME my-client-image
 else
     echo "Failed to build client image. Check the output for errors."
     exit 1
 fi
+
+# Print the machine name and pause for a keystroke
+echo "Machine Name: $MACHINE_NAME"
+echo "Press any key to exit..."
+read -n 1 -s
